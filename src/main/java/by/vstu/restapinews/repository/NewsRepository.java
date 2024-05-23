8b7f3dc16b6bc08d013b143c11b10718a7c6df14
Vitaliy-54 <112825723+Vitaliy-54@users.noreply.github.com>
2024-05-23 22:56:35 +0300

    Delete src/main directory

--- src/main/java/by/vstu/restapinews/repository/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package by.vstu.restapinews.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import by.vstu.restapinews.model.News;
-
-public interface NewsRepository extends CrudRepository<News, Long> {
-}
