60c8eae0eb4522406b84801f5941924ce34f1a2a
Vitaliy-54 <112825723+Vitaliy-54@users.noreply.github.com>
2024-06-01 13:32:04 +0200

    zachita.java

--- RestApiNews_no_html/src/main/java/RestApiNews/service/NewsService.java
+++ RestApiNews_no_html/src/main/java/RestApiNews/service/NewsService.java
@@ -1,6 +1,8 @@
 package RestApiNews.service;
 
 import RestApiNews.entity.News;
+import java.util.List;
 
 public interface NewsService extends Service<News> {
+   List<News> searchNewsByWord(String word);
 }
