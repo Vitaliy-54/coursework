e70118a79d280f259859221c1a1a46ba4c3b2fe5
Vitaliy-54 <112825723+Vitaliy-54@users.noreply.github.com>
2024-06-01 13:34:28 +0200

    zachita.java

--- RestApiNews_no_html/src/main/java/RestApiNews/repository/NewsRepository.java
+++ RestApiNews_no_html/src/main/java/RestApiNews/repository/NewsRepository.java
@@ -1,9 +1,15 @@
+
 package RestApiNews.repository;
 
 import RestApiNews.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
+  @Query("SELECT n FROM News n WHERE LOWER(n.content) LIKE LOWER(CONCAT('%', :word, '%'))")
+  List<News> searchNewsByWord(@Param("word") String word);
 }
